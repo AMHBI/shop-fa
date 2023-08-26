78d60ec5b8b6de1e88d116fdf66f1c47f0c0b67f
AMHBI <alimhbi1@gmail.com>
2023-08-26 17:47:06 +0330

    Complete Product components

--- src/Components/Shared/Product.js
+++ src/Components/Shared/Product.js
@@ -1,8 +1,16 @@
-import React from "react";
-import { titleShortner } from "../../Helper/fuctions";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 
+//Functions :
+import { titleShortner, isInCart, quantityCount } from "../../Helper/fuctions";
+
+//Contexts:
+import { CartContext } from "../../Context/CartContextProvider";
+//Icons
+import trashIcon from "../../assets/icon/trash.svg";
 const Product = ({ productData }) => {
+  const { state, dispatch } = useContext(CartContext);
+
   return (
     <div>
       <img src={productData.image} alt='product' style={{ width: "100px" }} />
@@ -11,7 +19,24 @@ const Product = ({ productData }) => {
       <div>
         <Link to={`/products/${productData.id}`}>Details</Link>
         <div>
-          <button>Add to Cart</button>
+          {isInCart(state, productData.id) ? (
+            <button
+              onClick={() =>
+                dispatch({ type: "INCREASE", payload: productData })
+              }>
+              +
+            </button>
+          ) : (
+            <button
+              onClick={() =>
+                dispatch({ type: "ADD_ITEM", payload: productData })
+              }>
+              Add To Cart
+            </button>
+          )}
+          {quantityCount(state,productData.id) === 1 && <button onClick={()=> dispatch({type:"REMOVE_ITEM",payload:productData})}><img src={trashIcon} alt="trash" style={{width:"15px"}}/></button>}
+          {quantityCount(state,productData.id) > 1 && <button onClick={()=> dispatch({type:"DECREASE",payload:productData})}>-</button>}
+          
         </div>
       </div>
     </div>
--- src/Helper/fuctions.js
+++ src/Helper/fuctions.js
@@ -2,8 +2,16 @@ export const titleShortner = title => {
     const splitedTitle = title.split(" ");
     const newTitle = `${splitedTitle[0]} ${splitedTitle[1]} ${splitedTitle[2]}`;
     return newTitle;
-}
+};
 export const isInCart = (state,id) =>{
     let result = !!state.selectedItems.find(item=> item.id === id);
     return result;
-}
+};
+export const quantityCount = (state,id) =>{
+    const index = state.selectedItems.findIndex(item => item.id === id)
+    if (index === -1) {
+        return false
+    } else {
+        return state.selectedItems[index].quantity; 
+    }
+};
