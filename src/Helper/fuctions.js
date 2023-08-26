ea84e55cc283819849f8d911b31e2f7a3e06125b
AMHBI <alimhbi1@gmail.com>
2023-08-26 17:23:35 +0330

    Adding in cart checker function

--- src/Helper/fuctions.js
+++ src/Helper/fuctions.js
@@ -3,3 +3,7 @@ export const titleShortner = title => {
     const newTitle = `${splitedTitle[0]} ${splitedTitle[1]} ${splitedTitle[2]}`;
     return newTitle;
 }
+export const isInCart = (state,id) =>{
+    let result = !!state.selectedItems.find(item=> item.id === id);
+    return result;
+}
