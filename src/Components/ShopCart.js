1c8a45310a32d0e64010d9ca9b14aa8884a68d66
AMHBI <alimhbi1@gmail.com>
2023-08-27 18:19:37 +0330

    Starting Cart Component

--- src/Components/ShopCart.js
+++ src/Components/ShopCart.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React , { useContext }from 'react';
+
+//Contexts : 
+import {CartContext} from '../Context/CartContextProvider';
+//Components :
+import Cart from './Shared/Cart';
 
 const ShopCart = () => {
+    const {state, dispatch} = useContext(CartContext);
     return (
         <div>
-            Shop Cart
+            <div>
+                {
+                    state.selectedItems.map(item => <Cart key={item.id} data={item}/>)
+                }
+            </div>
         </div>
     );
 };
